e2c798e255a143b3196eaaa26820e9b1f99b9224
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2022-02-08 22:02:42 +0100

    Move population with defaults in `preference` into separate file

---
+++ sql/preference-defaults.sql
@@ -0,0 +1,50 @@
+-- Default data for the "preference" table.
+
+-- To load from the command line:
+--
+--      sqlite3 database.db <preference-defaults.sql
+
+PRAGMA foreign_keys = ON;
+
+BEGIN;
+
+-- "Default days"
+-- https://github.com/NBISweden/urdr/issues/22
+--
+-- A user may have a default report for a day.  The preference entry
+-- for a default days has the abbreviated name of the day as "name",
+-- and the report for that day as a string that encodes a JSON array
+-- with "{ issue: ..., activity: ..., hours: ... }" entries (where
+-- "issue" and "activity" are Redmine internal identifiers for issues
+-- and activities).
+
+INSERT INTO preference (name, value) VALUES
+	("Mon", NULL),
+	("Tue", NULL),
+	("Wed", NULL),
+	("Thu", NULL),
+	("Fri", NULL),
+	("Sat", NULL),
+	("Sun", NULL);
+
+-- "Tabbing behaviour"
+-- https://github.com/NBISweden/urdr/issues/4
+--
+-- Pressing tab to go to the next input field could take you to the next
+-- field to the right, or to the next field below.  A user may choose
+-- whether to move horizontally or vertically.
+
+INSERT INTO preference (name, value) VALUES
+	("tab", "horizontal");	-- or "vertical"
+
+-- "Default input view (or behavior)"
+-- https://github.com/NBISweden/urdr/issues/23
+--
+-- A user's default way of inputting their time log may be "daily" or
+-- "weekly".
+
+INSERT INTO preference (name, value) VALUES
+
+	("input", "daily");	-- or "weekly"
+
+COMMIT;
--- sql/schema.sql
+++ sql/schema.sql
@@ -82,44 +82,4 @@ CREATE TABLE favorite (
 		ON CONFLICT REPLACE
 );
 
--- Insert default preference values:
-
--- "Default days"
--- https://github.com/NBISweden/urdr/issues/22
---
--- A user may have a default report for a day.  The preference entry
--- for a default days has the abbreviated name of the day as "name",
--- and the report for that day as a string that encodes a JSON array
--- with "{ issue: ..., activity: ..., hours: ... }" entries (where
--- "issue" and "activity" are Redmine internal identifiers for issues
--- and activities).
-
-INSERT INTO preference (name, value) VALUES
-	("Mon", NULL),
-	("Tue", NULL),
-	("Wed", NULL),
-	("Thu", NULL),
-	("Fri", NULL),
-	("Sat", NULL),
-	("Sun", NULL);
-
--- "Tabbing behaviour"
--- https://github.com/NBISweden/urdr/issues/4
---
--- Pressing tab to go to the next input field could take you to the next
--- field to the right, or to the next field below.  A user may choose
--- whether to move horizontally or vertically.
-
-INSERT INTO preference (name, value) VALUES
-	("tab", "horizontal");	-- or "vertical"
-
--- "Default input view"
--- https://github.com/NBISweden/urdr/issues/23
---
--- A user's default way of inputting their time log may be "daily" or
--- "weekly".
-
-INSERT INTO preference (name, value) VALUES
-	("input", "daily");	-- or "weekly"
-
 COMMIT;
