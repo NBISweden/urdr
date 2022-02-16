a43e4a2de418380d8bd17aaf0ab0cf8eb999877f
Henrike <henrike.wiemker@t-online.de>
2022-02-16 09:43:01 +0100

    move reporter and login to pages folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import { Login } from "./login";
-import { Reporter } from "./reporter";
+import { Login } from "./pages/login";
+import { Reporter } from "./pages/reporter";
 
 export const App = () => {
   return(
