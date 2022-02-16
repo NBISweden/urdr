19254e782a9a5f00ca3ce4f926f2d505c00351df
Henrike <henrike.wiemker@t-online.de>
2022-02-16 11:44:02 +0100

    format with Prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Reporter } from "./pages/reporter";
 
 export const App = () => {
-  return(
+  return (
     <>
       <Router>
-          <Switch>
-            <Route exact path = "/">
-              <Redirect to="/login"/>
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/timelog">
-              <Reporter />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/timelog">
+            <Reporter />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
-}
+};
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const Header = () => {
-  return(
+  return (
     <section>
       <h1>Welcome to urdr</h1>
     </section>
-  )
-}
+  );
+};
