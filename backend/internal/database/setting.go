69968526937fde7d4af4abfbcd1b552b1a6b57d0
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2022-02-19 20:06:26 +0100

    Rename functions, add `SetUserSetting()` and `getSetting()`

--- backend/internal/database/setting.go
+++ backend/internal/database/setting.go
@@ -9,21 +9,73 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// GetSetting returns the user-specific value for a particular setting.
-// If there is no user-specific value stored for the given user, then
-// the default value is returned, if there is one.
-func GetSetting(user int, setting string) (string, error) {
-        // SQL SELECT statement that fetches the value stored in
-        // user_setting given the Redmine user ID and the setting name.
-        // We also retrieve the default value for the setting at the
-        // same time.
+// getSetting is an internal function that returns the setting_id and
+// the value stored for a setting, given the setting's name.  It returns
+// an error for illegal settings.
+func getSetting(settingName string) (int, string, error) {
 	sqlStmt := `
-		SELECT	us.redmine_user_id, us.value, s.value
-		FROM	setting AS s
-		LEFT JOIN	user_setting AS us USING (setting_id)
-		WHERE	(us.redmine_user_id = ? OR
-			 us.redmine_user_id IS NULL)
-		AND	s.name = ?`
+		SELECT	setting_id, value
+		FROM	setting
+		WHERE	name = ?`
+
+	stmt, err := db.Prepare(sqlStmt)
+	if err != nil {
+		log.Error(err)
+		return 0, "", errors.New("db.Prepare() failed")
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(settingName)
+	if err != nil {
+		log.Error(err)
+		return 0, "", errors.New("stmt.Query() failed")
+	}
+	defer rows.Close()
+
+	isValidSetting := false
+	var settingId int
+	var settingValue sql.NullString
+
+	for rows.Next() {
+		err = rows.Scan(&settingId, &settingValue)
+		if err != nil {
+			log.Error(err)
+			return 0, "", errors.New("rows.Scan() failed")
+		}
+
+		isValidSetting = true
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Error(err)
+		return 0, "", errors.New("rows.Next() failed")
+	}
+
+	if !isValidSetting {
+		return 0, "", errors.New(fmt.Sprintf("Illegal setting name: %s\n", settingName))
+	}
+
+	if !settingValue.Valid {
+		return settingId, "", nil
+	}
+	return settingId, settingValue.String, nil
+}
+
+// GetUserSetting returns the user-specific value for a particular
+// setting.  If there is no user-specific value stored for the given
+// user, then the default value is returned, if there is one.
+func GetUserSetting(redmineUserId int, settingName string) (string, error) {
+	settingId, settingValue, err := getSetting(settingName)
+	if err != nil {
+		log.Error(err)
+		return "", errors.New("getSettingId() failed")
+	}
+
+	sqlStmt := `
+		SELECT	value
+		FROM	user_setting
+		WHERE	redmine_user_id = ?
+		AND	setting_id = ?`
 
 	stmt, err := db.Prepare(sqlStmt)
 	if err != nil {
@@ -32,36 +84,24 @@ func GetSetting(user int, setting string) (string, error) {
 	}
 	defer stmt.Close()
 
-	rows, err := stmt.Query(user, setting)
+	rows, err := stmt.Query(redmineUserId, settingId)
 	if err != nil {
 		log.Error(err)
 		return "", errors.New("stmt.Query() failed")
 	}
 	defer rows.Close()
 
-	isValidSetting := false
-	var dbValue sql.NullString
+	settingFound := false
+	var userSettingValue sql.NullString
 
 	for rows.Next() {
-                // The default value will be used if the user-specific
-                // value is NULL.
-		var dbDefaultValue sql.NullString
-		var dbUser sql.NullString
-
-		err = rows.Scan(&dbUser, &dbValue, &dbDefaultValue)
+		err = rows.Scan(&userSettingValue)
 		if err != nil {
 			log.Error(err)
 			return "", errors.New("rows.Scan() failed")
 		}
 		
-                // If the user ID coming back from the database is
-                // NULL, then there is no user-specific value for this
-                // setting.
-		if !dbUser.Valid {
-			dbValue = dbDefaultValue
-		}
-
-		isValidSetting = true
+		settingFound = true
 	}
 	err = rows.Err()
 	if err != nil {
@@ -69,12 +109,46 @@ func GetSetting(user int, setting string) (string, error) {
 		return "", errors.New("rows.Next() failed")
 	}
 
-	if !isValidSetting {
-		return "", errors.New(fmt.Sprintf("Invalid setting: %s", setting))
+	if !settingFound {
+		return settingValue, nil
 	}
 
-	if dbValue.Valid {
-		return dbValue.String, nil
+	if !userSettingValue.Valid {
+		return "", nil
 	}
-	return "", nil
+
+	return userSettingValue.String, nil
+}
+
+// SetUserSetting assigns the user-specific value for a particular
+// setting.  If there is already a user-specific value stored for the
+// given setting, the new value replaces the old value.
+func SetUserSetting(redmineUserId int, settingName string, userSettingValue string) error {
+	settingId, _, err := getSetting(settingName)
+	if err != nil {
+		log.Error(err)
+		return errors.New("getSettingId() failed")
+	}
+
+	sqlStmt := `
+		INSERT INTO user_setting (redmine_user_id, setting_id, value)
+		VALUES (?, ?, ?)`
+
+	stmt, err := db.Prepare(sqlStmt)
+	if err != nil {
+		log.Error(err)
+		return errors.New("db.Prepare() failed")
+	}
+	defer stmt.Close()
+
+	var res sql.Result
+	res, err = stmt.Exec(redmineUserId, settingId, userSettingValue)
+	if err != nil {
+		log.Error(err)
+		return errors.New("db.Exec() failed")
+	}
+
+	n, _ := res.RowsAffected()
+	log.Printf("Rows affected = %d\n", n)
+	return nil
 }
