eab00b8cb82470235d80a1b952e5182a66dda785
Henrike <henrike.wiemker@t-online.de>
2022-02-24 10:42:02 +0100

    add Reporter page
    copying old code, needs cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
+import { Reporter } from "./pages/Reporter";
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/report" element={<Reporter />} />
           </Routes>
         </BrowserRouter>
       </React.StrictMode>
---
+++ frontend/src/pages/Reporter.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+export interface TimeEntry {
+  issue_id: number;
+  activity_id: number;
+  hours: number;
+  comments: string;
+  spent_on: Date;
+  user_id: number;
+}
+
+export function Reporter() {
+  //used default values to speed testing
+  const [hours, set_hours] = useState(5 as number);
+  const [date, set_date] = useState("2022-02-16" as string);
+  const [issue, set_issue] = useState(0 as number);
+  const [activity, set_activity] = useState(18 as number);
+
+  let headers = new Headers();
+  headers.set("Accept", "application/json");
+  headers.set("Content-Type", "application/json");
+  const [issues, setIssues] = useState([]);
+
+  const IssuesList = () => {
+    return (
+      <>
+        <div>
+          <span>Issue</span> :
+          {issues && issues.length > 0 && (
+            <select onChange={(e) => set_issue(e.target.value)}>
+              {issues.map((data, index) => {
+                return <option value={data.id}>{data.subject}</option>;
+              })}
+            </select>
+          )}
+        </div>
+      </>
+    );
+  };
+
+  React.useEffect(() => {
+    fetch("http://localhost:8080/api/issues", {
+      method: "GET",
+      credentials: "include",
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.issues);
+        setIssues(data.issues);
+        if (data.issues.length > 0) set_issue(data.issues[0].id);
+      });
+  }, []);
+
+  function reportTime() {
+    // We are not doing account linking
+
+    let timeLog: TimeEntry = {
+      issue_id: Number(issue),
+      activity_id: Number(activity),
+      hours: Number(hours),
+      comments: "",
+      spent_on: date,
+      user_id: 232,
+    };
+    console.log(timeLog);
+
+    fetch("http://localhost:8080/api/report", {
+      body: JSON.stringify(timeLog),
+      method: "POST",
+      credentials: "include",
+      headers: headers,
+    }).then((response) => {
+      if (response.ok) {
+        console.log("Time reported");
+      } else {
+        console.log("Time report failed");
+      }
+    });
+  }
+
+  return <input type="button" onClick={reportTime} value="Report time" />;
+}
