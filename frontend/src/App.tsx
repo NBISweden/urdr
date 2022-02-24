63084c768fb304a11ed5b5c74b3e5623603ddabd
Henrike <henrike.wiemker@t-online.de>
2022-02-24 10:27:30 +0100

    add Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/Login";
 
 export const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </>
+  );
 };
