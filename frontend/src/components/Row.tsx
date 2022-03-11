aa7863286bb90e6109fffbbb17bdd54a7f8bfe06
Henrike <henrike.wiemker@t-online.de>
2022-03-11 14:38:03 +0100

    correct spreadsheet behavior for time entries

--- frontend/src/components/Row.tsx
+++ frontend/src/components/Row.tsx
@@ -16,25 +16,27 @@ export const Row = ({
   onCellUpdate: (timeEntry: TimeEntry) => void;
 }) => {
   const [rowEntries, setRowEntries] = useState<FetchedTimeEntry[]>([]);
-  const [rowHours, setRowHours] = useState<number[]>([]);
+  const [rowHours, setRowHours] = useState<number[]>([0, 0, 0, 0, 0]);
   const [rowEntryIds, setRowEntryIds] = useState<number[]>([]);
+
   let headers = new Headers();
   headers.set("Accept", "application/json");
   headers.set("Content-Type", "application/json");
+
   let params = new URLSearchParams({
     issue_id: `${recentIssue.issue.id}`,
     activity_id: `${recentIssue.activity.id}`,
-    start_date: `2022-03-07`,
-    end_date: `2022-03-11`,
+    start_date: `${days[0].toISOString().split("T")[0]}`,
+    end_date: `${days[4].toISOString().split("T")[0]}`,
   });
+
   const getTimeEntries = async (params: URLSearchParams) => {
-    let entries: FetchedTimeEntry[] = await fetch(
+    let entries: { time_entries: FetchedTimeEntry[] } = await fetch(
       `http://localhost:8080/api/time_entries?${params}`,
       {
         method: "GET",
         credentials: "include",
         headers: headers,
-        //we somehow need to send id's and date's here
       }
     )
       .then((res) => {
@@ -48,17 +50,17 @@ export const Row = ({
       .catch((error) => console.log(error));
     setRowEntries(entries.time_entries);
   };
+
   React.useEffect(() => {
     getTimeEntries(params);
   }, []);
 
   const findCurrentHours = (day: Date) => {
-    console.log("finding hours", rowEntries);
     let hours = 0;
     let entry: TimeEntry | FetchedTimeEntry = rowUpdates?.find(
       (entry) => entry.spent_on === day.toISOString().split("T")[0]
     );
-    if (!entry) {
+    if (!entry && rowEntries && rowEntries.length > 0) {
       entry = rowEntries?.find(
         (entry) => entry.spent_on === day.toISOString().split("T")[0]
       );
@@ -69,19 +71,7 @@ export const Row = ({
     return hours;
   };
 
-  React.useEffect(() => {
-    console.log("use effect");
-    if (rowEntries && rowEntries.length > 0) {
-      console.log("if true");
-      const hours = days.map((day) => findCurrentHours(day));
-      setRowHours(hours);
-      const entryIds = days.map((day) => findEntryId(day));
-      setRowEntryIds(entryIds);
-    }
-  }, [rowEntries, rowUpdates]);
-
   const findEntryId = (day: Date) => {
-    console.log("finding entry", rowEntries);
     let id = 0;
     let entry = rowEntries?.find(
       (entry) => entry.spent_on === day.toISOString().split("T")[0]
@@ -91,6 +81,14 @@ export const Row = ({
     }
     return id;
   };
+
+  React.useEffect(() => {
+    const hours = days.map((day) => findCurrentHours(day));
+    setRowHours(hours);
+    const entryIds = days.map((day) => findEntryId(day));
+    setRowEntryIds(entryIds);
+  }, [rowEntries, rowUpdates]);
+
   return (
     <>
       <div className="row issue-row">
