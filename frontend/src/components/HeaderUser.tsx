eae3da186c219104f6aa427854378770f8491a5f
jonandernovella <novella.uu@gmail.com>
2022-03-29 15:51:53 +0200

    Add HeaderUser component with logout option
    This is a dropdown showing the name of the logged in user and the possibility to log out.

---
+++ frontend/src/components/HeaderUser.tsx
@@ -0,0 +1,58 @@
+import "../index.css";
+import React, { useState } from "react";
+import { SNOWPACK_PUBLIC_API_URL } from "../model";
+
+const logout = async () => {
+  await fetch(`${SNOWPACK_PUBLIC_API_URL}/api/logout`, {
+    method: "POST",
+  })
+    .then((res) => {
+      if (res.ok) {
+        return true;
+      } else {
+        throw new Error("Could not log out.");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+  // Redirect to login page
+  window.location.href = "/";
+};
+
+const showSettings = () => {
+  return (
+    <div class="settings-list" aria-labelledby="dropdownMenuLink">
+      <a class="dropdown-item">My settings</a>
+      <a class="dropdown-item" onClick={logout}>
+        Log out
+      </a>
+    </div>
+  );
+};
+
+export const HeaderUser = ({ username }: { username: string }) => {
+  const [userSettings, setUserSettings] = useState<boolean>(false);
+
+  const showSettingsClickHandle = () => {
+    setUserSettings(!userSettings);
+  };
+
+  return (
+    <div className="d-flex justify-content-end header-user">
+      <div className="p-2">
+        <a
+          class="btn basic-button dropdown-toggle"
+          onClick={showSettingsClickHandle}
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          {username}
+        </a>
+        {userSettings ? showSettings() : null}
+      </div>
+    </div>
+  );
+};
