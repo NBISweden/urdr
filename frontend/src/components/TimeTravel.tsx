94f12a898a58d5f924a4e4ae1959c52002409636
jonandernovella <novella.uu@gmail.com>
2022-04-01 19:40:34 +0200

    Add with portal

--- frontend/src/components/TimeTravel.tsx
+++ frontend/src/components/TimeTravel.tsx
@@ -65,9 +65,9 @@ export const TimeTravel = ({
         customInput={<CustomDatePickerInput />}
         startDate={currentWeekArray[0]}
         endDate={currentWeekArray[4]}
-        selected={weekTravelDay}
         selectsRange
         locale={sv}
+        withPortal
       />
       <a
         onClick={nextWeeksClickHandle}
