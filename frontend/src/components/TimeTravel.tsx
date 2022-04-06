3a4606e752816504bd0b3e04062084393e768f9b
Henrike <henrike.wiemker@t-online.de>
2022-04-06 09:56:00 +0200

    finish top bar

--- frontend/src/components/TimeTravel.tsx
+++ frontend/src/components/TimeTravel.tsx
@@ -41,7 +41,17 @@ export const TimeTravel = ({
   };
 
   const CustomDatePickerInput = forwardRef(({ onClick }, ref) => (
-    <button onClick={onClick} className="header-time-travel" ref={ref}>
+    <button onClick={onClick} className="week-button" ref={ref}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+      </svg>
       Week {`${currentWeek}`}
     </button>
   ));
@@ -53,14 +63,10 @@ export const TimeTravel = ({
 
   return (
     <div className="time-travel">
-      <button
-        onClick={previousWeeksClickHandle}
-        className="header-time-travel week-button"
-      >
+      <button onClick={previousWeeksClickHandle} className="week-arrow-button">
         <img src={left} alt="left arrow" className="week-arrow" />
       </button>
       <DatePicker
-        wrapperClassName="header-time-travel header-week"
         onChange={(date) => handleDateChange(date)}
         showWeekNumbers
         filterDate={isWeekday}
@@ -73,10 +79,7 @@ export const TimeTravel = ({
         todayButton="Idag"
         withPortal
       />
-      <button
-        onClick={nextWeeksClickHandle}
-        className="header-time-travel week-button"
-      >
+      <button onClick={nextWeeksClickHandle} className="week-arrow-button">
         <img src={right} alt="right arrow" className="week-arrow" />
       </button>
     </div>
