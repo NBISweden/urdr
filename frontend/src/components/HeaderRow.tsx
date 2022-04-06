db8347f2971e4d05761d7f8de5f929eda39d8aec
Henrike <henrike.wiemker@t-online.de>
2022-04-06 13:07:53 +0200

    make dates responsive

--- frontend/src/components/HeaderRow.tsx
+++ frontend/src/components/HeaderRow.tsx
@@ -1,18 +1,42 @@
 import React from "react";
+import {
+  getLongCustomDateString,
+  getShortCustomDateString,
+  useViewport,
+} from "../utils";
 
 export const HeaderRow = ({ days }: { days: Date[] }) => {
-  const dayStrings = days.map((day) => {
-    return day.toISOString().split("T")[0];
+  const { width } = useViewport();
+  const breakpoint = 1044;
+  const longDayStrings = days.map((day) => {
+    return getLongCustomDateString(day);
   });
+
+  const shortDayStrings = days.map((day) => {
+    return getShortCustomDateString(day);
+  });
+
+  const Dates = ({ dayStrings }: { dayStrings: string[] }) => {
+    return (
+      <>
+        {dayStrings.map((day, index) => {
+          return (
+            <p key={day} className={index === 0 ? "col-1 offset-6" : "col-1"}>
+              {day}
+            </p>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <div className="row">
-      {dayStrings.map((day, index) => {
-        return (
-          <p key={day} className={index === 0 ? "col-1 offset-6" : "col-1"}>
-            {day}
-          </p>
-        );
-      })}
+      {width >= breakpoint ? (
+        <Dates dayStrings={longDayStrings} />
+      ) : (
+        <Dates dayStrings={shortDayStrings} />
+      )}
     </div>
   );
 };
