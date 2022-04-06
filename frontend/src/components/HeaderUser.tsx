7d5129b2ab0318e436e7dd28a41c35bf220b4511
Henrike <henrike.wiemker@t-online.de>
2022-04-06 14:38:03 +0200

    fix settings dropdown

--- frontend/src/components/HeaderUser.tsx
+++ frontend/src/components/HeaderUser.tsx
@@ -24,11 +24,13 @@ const logout = async () => {
 
 const showSettings = () => {
   return (
-    <div className="settings-list" aria-labelledby="dropdownMenuLink">
-      <a className="dropdown-item" onClick={logout}>
-        Log out
-      </a>
-    </div>
+    <ul className="settings-box" aria-labelledby="dropdownMenuLink">
+      <li className="settings-list-item">
+        <a className="dropdown-item settings-list-link" onClick={logout}>
+          Logout
+        </a>
+      </li>
+    </ul>
   );
 };
 
@@ -40,7 +42,7 @@ export const HeaderUser = ({ username }: { username: string }) => {
   };
 
   return (
-    <div className="p-2">
+    <div className="p-2 settings-wrapper">
       <a
         className="btn dropdown-button"
         onClick={showSettingsClickHandle}
@@ -49,15 +51,27 @@ export const HeaderUser = ({ username }: { username: string }) => {
         aria-expanded={showUserSettings}
       >
         {username}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-        </svg>
+        {showUserSettings ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+          </svg>
+        )}
       </a>
       {showUserSettings ? showSettings() : null}
     </div>
