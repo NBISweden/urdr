47290f843b86589a5bb77516b2db79830ed6b475
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2022-04-28 13:31:41 +0200

    Start of database API for invalid entries

---
+++ backend/internal/database/invalidEntry.go
@@ -0,0 +1,49 @@
+package database
+
+import "fmt"
+
+// invalidActivities is a map that maps Redmine issue IDs onto arrays of
+// invalid Redmine activity IDs for that issue ID.
+var invalidActivities map[int][]int
+
+// loadAllInvalidEntries() loads the static list of invalid combinations
+// of Redmine issue IDs and Redmine activity IDs from the backend
+// database into the package-global invalidActivities structure.  This
+// map is later used by IsInvalidEntry().
+func (db *Database) loadAllInvalidEntries() error {
+	selectStmt := `
+		SELECT	redmine_issue_id,
+				redmine_activity_id
+		FROM	invalid_entry`
+
+	stmt, err := db.handle().Prepare(selectStmt)
+	if err != nil {
+		return fmt.Errorf("sql.Prepare() failed: %w", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return fmt.Errorf("sql.Query() failed: %w", err)
+	}
+	defer rows.Close()
+
+	invalidActivities = make(map[int][]int)
+
+	for rows.Next() {
+		var redmineIssueId int
+		var redmineActivityId int
+
+		if err := rows.Scan(&redmineIssueId, &redmineActivityId); err != nil {
+			return fmt.Errorf("sql.Scan() failed: %w", err)
+		}
+
+		invalidActivities[redmineIssueId] =
+			append(invalidActivities[redmineIssueId], redmineActivityId)
+	}
+	if err := rows.Err(); err != nil {
+		return fmt.Errorf("sql.Next() failed: %w", err)
+	}
+
+	return nil
+}
