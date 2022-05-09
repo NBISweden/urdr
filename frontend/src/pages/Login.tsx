74bec34d05e95b0e00a3198145691aedaee3ab92
Henrike <henrike.wiemker@t-online.de>
2022-05-09 16:34:07 +0200

    add basic error message for 401

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,45 +8,51 @@ import { AuthContext } from "../components/AuthProvider";
 export const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
   const { onLogin } = React.useContext(AuthContext);
 
   const authenticateRedmine = async (event) => {
     event?.preventDefault();
-    const user = await onLogin(username, password);
+    const loginResponse = await onLogin(username, password);
 
-    if (!user) {
-      setUsername("");
-      setPassword("");
-      console.log("Something went wrong!");
+    if (loginResponse === 401) {
+      setShowError(true);
       return;
     }
     navigate("/report");
   };
 
   return (
-    <div className="login-wrapper">
-      <LoginHeader />
-      <form onSubmit={authenticateRedmine} className="login-form">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          placeholder="Enter your username"
-          autoFocus
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <label htmlFor="Password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          placeholder="Enter you password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <input type="submit" value="Login" className="login-button" />
-      </form>
-    </div>
+    <main>
+      <div className="login-wrapper">
+        <LoginHeader />
+        <form onSubmit={authenticateRedmine} className="login-form">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            placeholder="Enter your username"
+            autoFocus
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <label htmlFor="Password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            placeholder="Enter you password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <input type="submit" value="Login" className="login-button" />
+        </form>
+        {showError && (
+          <div>
+            <p>Wrong combination of username and password.</p>
+          </div>
+        )}
+      </div>
+    </main>
   );
 };
