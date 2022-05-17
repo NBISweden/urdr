8633388e34ab6553f05a5b3cd7121a5965068337
Henrike <henrike.wiemker@t-online.de>
2022-05-17 11:26:37 +0200

    make toast reusable for success messages

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -1,17 +1,45 @@
 import React, { useEffect } from "react";
-import xgreen from "../icons/x-green.svg";
 
-export const Toast = ({ onCloseToast }: { onCloseToast: () => void }) => {
+type ToastType = "success" | "warning" | "info";
+
+export const Toast = ({
+  onCloseToast,
+  type,
+  timeout,
+  message,
+}: {
+  onCloseToast: () => void;
+  type: ToastType;
+  timeout: number;
+  message: string;
+}) => {
   useEffect(() => {
     setTimeout(() => {
       onCloseToast();
-    }, 3000);
+    }, timeout);
   });
+
   return (
-    <div className="toast-box" role="status">
-      <p className="toast-message">All changes saved!</p>
-      <button onClick={onCloseToast} className="toast-button">
-        <img src={xgreen} alt="close button" />
+    <div
+      className={`toast-box toast-${type}`}
+      role={type === "success" || type === "info" ? "status" : "alert"}
+    >
+      <p className="toast-message">{message}</p>
+      <button
+        onClick={onCloseToast}
+        className="toast-button"
+        aria-label="close message"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-x"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+        </svg>
       </button>
     </div>
   );
