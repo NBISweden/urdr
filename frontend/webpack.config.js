c8a8e04c5d51518b013feecaffd85cfade31a5d9
jonandernovella <novella.uu@gmail.com>
2022-07-07 13:41:15 +0200

    Format webpack conf accordingly

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
-var webpack = require('webpack');
+const path = require("path");
+var webpack = require("webpack");
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.tsx',
-   },
-    module: {
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.tsx",
+  },
+  module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
         exclude: ["/node_modules/"],
         options: {
-          transpileOnly: true
+          transpileOnly: true,
         },
       },
       {
@@ -23,14 +23,14 @@ var webpack = require('webpack');
       {
         test: /\.(css)$/i,
         use: ["style-loader", "css-loader"],
-      }
+      },
     ],
   },
-   resolve: {
+  resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
   },
-   devtool: 'inline-source-map',
-   devServer: {
+  devtool: "inline-source-map",
+  devServer: {
     open: true,
     host: "0.0.0.0",
     port: 4242,
@@ -39,17 +39,19 @@ var webpack = require('webpack');
     historyApiFallback: true,
     static: "./public",
     client: {
-      webSocketURL: 'ws://localhost:4567/ws',
+      webSocketURL: "ws://localhost:4567/ws",
     },
   },
-   plugins: [
+  plugins: [
     new webpack.DefinePlugin({
-      'process.env.PUBLIC_API_URL': JSON.stringify(process.env.PUBLIC_API_URL),
-      'process.env.PUBLIC_REDMINE_URL': JSON.stringify(process.env.PUBLIC_REDMINE_URL),
-    })
-   ],
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, './public')
-   },
- };
+      "process.env.PUBLIC_API_URL": JSON.stringify(process.env.PUBLIC_API_URL),
+      "process.env.PUBLIC_REDMINE_URL": JSON.stringify(
+        process.env.PUBLIC_REDMINE_URL
+      ),
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./public"),
+  },
+};
