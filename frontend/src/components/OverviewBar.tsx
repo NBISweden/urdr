8f56c8779dfe5b08a6d061aa0847e3e232ae2c6f
Henrike <henrike.wiemker@t-online.de>
2022-08-24 14:08:59 +0200

    styled bar at top of page

--- frontend/src/components/OverviewBar.tsx
+++ frontend/src/components/OverviewBar.tsx
@@ -3,8 +3,9 @@ import chart from "../images/barchart_mock_notext.png";
 
 export const OverviewBar = () => {
   return (
-    <section>
-      <img src={chart} />
+    <section className="overview-wrapper">
+      <h2 className="overview-heading">This year's work</h2>
+      <img src={chart} className="overview-bar" />
     </section>
   );
 };
