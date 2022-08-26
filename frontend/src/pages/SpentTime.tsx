58a35f125f12a4b517b50899aa4d6f220a200d86
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-08-26 14:28:49 +0200

    Issue #576: Fixing warning and formatting

--- frontend/src/pages/SpentTime.tsx
+++ frontend/src/pages/SpentTime.tsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import { AuthContext } from "../components/AuthProvider";
 import { getTimeEntries } from "../utils";
 import { FetchedTimeEntry } from "model";
-import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from "react-minimal-pie-chart";
+import { Data } from "react-minimal-pie-chart/types/commonTypes";
 
 export const SpentTime = () => {
   const [spentTime, setSpentTime] = useState<{}>({});
@@ -15,7 +16,7 @@ export const SpentTime = () => {
     getHoursPerActivity();
   }, []);
 
-  let data = [];
+  let data: Data | { color: string; title: string; value: any }[] = [];
 
   const getHoursPerActivity = async () => {
     const timeEntries = await getTimeEntries(
@@ -24,28 +25,26 @@ export const SpentTime = () => {
       today,
       context
     );
-    let activityHours = {};
+    let activityHours: { [key: string]: any } = {};
+
     timeEntries.map((entry: FetchedTimeEntry) => {
       if (!activityHours[entry.activity.name]) {
         activityHours[entry.activity.name] = 0;
       }
       activityHours[entry.activity.name] += entry.hours;
     });
-
-
+    console.log(activityHours);
     setSpentTime(activityHours);
-
-
   };
 
   // Example inspiration: https://medium.com/@tgknapp11/render-a-chart-with-react-minimal-pie-chart-e30420c9276c
-  let keys= Object.keys(spentTime)
+  let keys = Object.keys(spentTime);
   keys.map((key) => {
     let randomColor = "#000000".replace(/0/g, function () {
       return (~~(Math.random() * 16)).toString(16);
     });
 
-  let insert = {
+    let insert = {
       color: randomColor,
       title: key,
       value: spentTime[key],
@@ -59,17 +58,17 @@ export const SpentTime = () => {
     <>
       <main>
         <PieChart
-            data={ data }
-            style={{ height: '320px' }}
-            label={(data) => data.dataEntry.value}
-            labelStyle={{
-              fontSize: "0.40rem",
-              fontColor: "FFFFFA",
-              fontWeight: "800",
-            }}
-            radius={42}
-            labelPosition={112}
-        />;
+          data={data}
+          style={{ height: "320px" }}
+          label={(data) => data.dataEntry.value}
+          labelStyle={{
+            fontSize: "0.40rem",
+            fontWeight: "800",
+          }}
+          radius={42}
+          labelPosition={112}
+        />
+        ;
         <header>
           <h1>Spent time</h1>
         </header>
