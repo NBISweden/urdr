3ab5dc14926daff325266a08d2c40e3ff4e05445
Andreas Kähäri <andreas.kahari@nbis.se>
2025-04-22 16:50:43 +0200

    fix: Make it possible to access settings for one or several other users

--- backend/api/getSettingsHandler.go
+++ backend/api/getSettingsHandler.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
 )
@@ -12,9 +15,11 @@ import (
 // @Produce     json
 // @Success      200 {object} api.getSettingsHandler.settingResponse "User setting"
 // @Failure      401 {string} error "Unauthorized"
+// @Failure      400 {string} error "Bad Request"
 // @Failure      500 {string} error "Internal Server Error"
 // @Router       /api/setting [get]
 // @Param	name query string true "Key to get settings for"
+// @Param	user_id query int true "User ID(s) to get settings for (optional)"
 func getSettingsHandler(c *fiber.Ctx) error {
 	session, err := store.Get(c)
 	if err != nil {
@@ -22,25 +27,51 @@ func getSettingsHandler(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	userID := session.Get("user_id")
-	if userID == nil {
-		log.Error("Failed to get valid user ID from session")
-		return c.SendStatus(fiber.StatusUnauthorized)
-	}
+	var userIDs []int
 
-	name := c.Query("name")
-	value, err := db.GetUserSetting(userID.(int), name)
-	if err != nil {
-		log.Errorf("Failed to get user settings: %v", err)
-		return c.SendStatus(fiber.StatusInternalServerError)
+	// Getting setting for current user, or some other (list) of user(s)?
+	if c.Query("user_id") == "" {
+		// If no user_id is provided, use the current user's ID
+		userID := session.Get("user_id")
+		if userID == nil {
+			log.Error("Failed to get valid user ID from session")
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+		userIDs = append(userIDs, userID.(int))
+	} else {
+		// If user_id is provided, use it (split by comma)
+		userIDStrs := c.Query("user_id")
+		for _, userIDStr := range strings.Split(userIDStrs, ",") {
+			userID, err := strconv.Atoi(userIDStr)
+			if err != nil {
+				log.Errorf("Failed to convert user_id to int: %v", err)
+				return c.SendStatus(fiber.StatusBadRequest)
+			}
+			userIDs = append(userIDs, userID)
+		}
 	}
 
 	type settingResponse struct {
+		Id    int    `json:"id"`
 		Name  string `json:"name"`
 		Value string `json:"value"`
 	}
-	return c.JSON(settingResponse{
-		Name:  name,
-		Value: value,
-	})
+
+	var settings []settingResponse
+
+	name := c.Query("name")
+	for _, userID := range userIDs {
+		value, err := db.GetUserSetting(userID, name)
+		if err != nil {
+			log.Errorf("Failed to get user settings: %v", err)
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		settings = append(settings, settingResponse{
+			Id:    userID,
+			Name:  name,
+			Value: value,
+		})
+	}
+
+	return c.JSON(settings)
 }
