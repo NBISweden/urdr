14c144d8f4d49c48919c58c830e9d71f28bb3759
Jessica Sandler <jessica.sandler@nbis.se>
2025-08-28 18:07:51 +0200

    feat: add 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,24 @@ import { Login } from "./pages/Login";
 import { Report } from "./pages/Report";
 import { Help } from "./pages/Help";
 import { AbsencePlanner } from "./pages/AbsencePlanner";
+import { VacationOverview } from "./pages/VacationOverview";
+import { NotFound } from "./pages/NotFound";
 import { AuthProvider } from "./components/AuthProvider";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { getISOWeek, getISOWeekYear } from "date-fns";
 import { ConfirmDialogProvider } from "./components/ConfirmDialogProvider";
 import { EditPeriodDialogProvider } from "./components/EditPeriodDialogProvider";
-import {VacationOverview} from "./pages/VacationOverview";
 
 // Route calls
 // Order of routes is critical.
-// First check for /login, the for the expected /report/year/week.
+// First check is for the root path ("/"), which will redirect to the
+// report page with the current year and week.
+// Next check is for /login, then for the expected /report/year/week.
 // If none of these apply, redirect to /report/year/week using
 // current year and week and replace the history element.
+// If none of the routes match, the NotFound component will be rendered.
+
+
 export const App = () => {
   const currentYear: number = getISOWeekYear(new Date());
   const currentWeek: number = getISOWeek(new Date());
@@ -28,6 +34,15 @@ export const App = () => {
             <EditPeriodDialogProvider>
               <ConfirmDialogProvider>
                 <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <Navigate
+                        replace
+                        to={`/report/${currentYear}/${currentWeek}`}
+                      />
+                    }
+                  />
                   <Route path="/login" element={<Login />} />
                   <Route
                     path="/report/:year/:week"
@@ -38,7 +53,7 @@ export const App = () => {
                     }
                   />
                   <Route
-                    path="/*"
+                    path="/report"
                     element={
                       <Navigate
                         replace
@@ -62,14 +77,15 @@ export const App = () => {
                       </ProtectedRoute>
                     }
                   />
-                    <Route
-                        path="/vacation"
-                        element={
-                            <ProtectedRoute>
-                                <VacationOverview />
-                            </ProtectedRoute>
-                        }
-                    />
+                  <Route
+                    path="/vacation"
+                    element={
+                      <ProtectedRoute>
+                        <VacationOverview />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route path="*" element={<NotFound />} />
                 </Routes>
               </ConfirmDialogProvider>
             </EditPeriodDialogProvider>
