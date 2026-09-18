e642e432d0e8893a52a0b4274e21f35e125e0693
Jessica Sandler <jessica.sandler@nbis.se>
2026-09-18 16:42:01 +0200

    build: add node types for vite.config.ts and declare the define variables

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare const process: {
+  env: {
+    PUBLIC_API_URL: string;
+    PUBLIC_REDMINE_URL: string;
+    GIT_BRANCH: string;
+    GIT_HASH: string;
+  };
+};
